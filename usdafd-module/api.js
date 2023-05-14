84fe1ffcd3d9da816f03f8aede55f72ba3025cb0
Thrice64 <67223180+Thrice64@users.noreply.github.com>
2023-05-14 01:56:48 -0700

    searchID and search methods

--- usdafd-module/api.js
+++ usdafd-module/api.js
@@ -9,9 +9,9 @@ const base = 'https://api.nal.usda.gov/fdc/v1';
 const apiKey = ""; // api key here
 
 // search api function to search by phone number and/or country_code id
-const search= async (food) => {
+const searchID = async (food_id) => {
     try {
-        const verifyURL = `${base}/food/${food}?api_key=${apiKey}`;
+        const idURL = `${base}/food/${food_id}?api_key=${apiKey}`;
         
         const res = await superagent.get(verifyURL);
         console.log(res.body);
@@ -20,8 +20,21 @@ const search= async (food) => {
     }
 };
 
-search(1104067);
+const search = async (query) => {
+    try {
+       const queryURL = `${base}/foods/search?api_key=${apiKey}&query=${query}` 
+
+       const res = await superagent.get(queryURL);
+       console.log(res.body);
+    } catch (error) {
+        console.log(error);
+    }
+};
+// test
+//searchID(1104067);
+//search('Cheddar cheese');
 
 module.exports = {
+    searchID,
     search
 };
